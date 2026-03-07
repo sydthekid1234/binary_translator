d4999a29125e678b0e759e4bfdcd11c6d0db25ff
sydthekid1234 <sydthekidbetz@gmail.com>
2026-03-07 17:44:59 +0000

    maybe this will work?

--- basic_binary_translator.py
+++ basic_binary_translator.py
@@ -1,3 +1,7 @@
+def text_to_binary_ascii(text):
+    binary_list = [format(ord(char), '08b') for char in text]
+    return ' '.join(binary_list)
+
 message = input("What would you like to turn into Binary?")
 binary_output = text_to_binary_ascii(message)
 print(binary_output)
